354b468791620df3c96c5c1eb11087f4de8b769d
Brenda Silva <brendaalexandra1213@gmail.com>
2020-11-24 11:09:34 -0300

    Primeiro commit

---
+++ src/test/java/br/com/brendasilva/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brendasilva.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

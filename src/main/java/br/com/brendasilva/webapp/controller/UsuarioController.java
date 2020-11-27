91fc5611996b942a238319dc590c7ce8abb42656
Brenda Silva <brendaalexandra1213@gmail.com>
2020-11-27 15:43:46 -0300

    Tutorial parte 08 - funcionando

--- src/main/java/br/com/brendasilva/webapp/controller/UsuarioController.java
+++ src/main/java/br/com/brendasilva/webapp/controller/UsuarioController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
  
@@ -143,5 +144,22 @@ public class UsuarioController {
  
 		/*RETORNA A VIEW*/
 	    return new ModelAndView("consultarCadastros");
+	}
+	
+	/***
+	 * EXCLUI UM REGISTRO DO SISTEMA PELO CÓDIGO
+	 * @param codigoUsuario
+	 * @return
+	 */
+	@RequestMapping(value="/excluir", method= RequestMethod.POST)
+	public ModelAndView excluir(@RequestParam("codigoUsuario") Long codigoUsuario){
+ 
+		ModelAndView modelAndView = new ModelAndView("redirect:/usuario/consultar");
+ 
+		/*EXCLUINDO O REGISTRO*/
+		this.usuarioService.excluir(codigoUsuario);
+ 
+		/*RETORNANDO A VIEW*/
+		return modelAndView;
 	}
 }
